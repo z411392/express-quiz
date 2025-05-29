7f3fdf4f0dc724aa6884c091c4f8c028214a46ca
z411392 <z411392@gmail.com>
2025-05-29 12:31:31 +0800

    refactor: typo onDeleteUser -> onDeletingUser

--- lib/http.ts
+++ lib/http.ts
@@ -3,11 +3,11 @@ import cors from "cors"
 import { createServer } from "node:http"
 import { onCreatingUser } from "@/modules/user-managing/presentation/http/controllers/on-creating-user"
 import { onListingUsers } from "@/modules/user-managing/presentation/http/controllers/on-listing-users"
-import { onDeleteUser } from "@/modules/user-managing/presentation/http/controllers/on-deleting-user"
+import { onDeletingUser } from "@/modules/user-managing/presentation/http/controllers/on-deleting-user"
 
 const router = Router()
 
-router.post("/users", onCreatingUser).get("/users", onListingUsers).delete("/users/:userId", onDeleteUser)
+router.post("/users", onCreatingUser).get("/users", onListingUsers).delete("/users/:userId", onDeletingUser)
 
 export default () => {
     const app = express()
--- lib/modules/user-managing/presentation/http/controllers/on-deleting-user.ts
+++ lib/modules/user-managing/presentation/http/controllers/on-deleting-user.ts
@@ -2,7 +2,7 @@ import { type Request, type Response, type NextFunction } from "express"
 import { UserManager } from "@/adapters/mocked/user-manager"
 import { type DeletingUser, DeleteUser } from "@/modules/user-managing/application/mutations/delete-user"
 
-export const onDeleteUser = async (request: Request, response: Response, next: NextFunction) => {
+export const onDeletingUser = async (request: Request, response: Response, next: NextFunction) => {
     try {
         const { userId } = request.params
         const mutation: DeletingUser = { userId }
